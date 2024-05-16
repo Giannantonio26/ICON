05b48d309a7ac4acf87eae6c5e649d3744f4ea4b
Giannantonio Sanrocco <127191563+Giannantonio26@users.noreply.github.com>
2024-05-16 23:28:03 +0200

    modifca commenti

--- src/main.py
+++ src/main.py
@@ -32,16 +32,12 @@ def main():
     newDataset = loadDataset("new_dataset.csv")
     # rimuovo righe con valori nulli (perchè mi dava errore per righe con valori nulli)
     newDataset = newDataset.dropna()
-
     # Calcola il numero di righe da eliminare
     rows_to_drop = int(len(newDataset) * 0.70)
-
-    # Seleziona casualmente il n% delle righe da eliminare
+    # Seleziona casualmente le righe da mantenere
     rows_to_keep = newDataset.sample(n=len(newDataset) - rows_to_drop, random_state=42)
-
     # Crea un nuovo dataset con le sole righe selezionate
     newDataset = newDataset.loc[rows_to_keep.index]
-    
 
     selected_columns = [
         'Pos', 'TouDefPen', 'TouDef3rd', 'TouMid3rd', 'TouAtt3rd', 
@@ -53,16 +49,12 @@ def main():
     #discretizzo il dataset
     # Explicitly set subsample to None to silence the warning
     discretizer = KBinsDiscretizer(encode='ordinal', strategy='uniform', subsample=None)
-
     # Select columns of float64 and int64 types
     continuos_columns = newDataset.select_dtypes(include=['float64', 'int64']).columns
-
     # Apply the discretizer to the selected columns
     newDataset[continuos_columns] = discretizer.fit_transform(newDataset[continuos_columns])
-
     print(newDataset)
-
-    #Creo o leggo la rete bayesiana a seconda delle necessità
+    #Creazione o lettura della rete bayesiana in base alle necessità
     #bayesianNetwork = create_BN(newDataset)
     bayesianNetwork = loadBayesianNetwork()
 
